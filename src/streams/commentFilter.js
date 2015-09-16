f3b26364eb5b5d8004dcd5d0d4a6433df84d2669
Andrew Morris <voltrevo@gmail.com>
2015-09-17 08:21:47 +1000

    Add commentFilter

---
+++ src/streams/commentFilter.js
@@ -0,0 +1,79 @@
+'use strict';
+
+// TODO: block comments and implement via parser
+
+module.exports = function(inputStream) {
+  var stream = {};
+
+  stream.hasNext = function() {
+    if (!inputStream.hasNext()) {
+      return false;
+    }
+
+    var c = inputStream.peek();
+
+    if (c !== '/') {
+      return true;
+    }
+
+    var mark = inputStream.mark();
+
+    // Skip over the '/' we just peeked
+    inputStream.next();
+
+    var cc = inputStream.next();
+
+    if (cc !== '/') {
+      inputStream.restore(mark);
+      return true;
+    }
+
+    while (true) {
+      if (!inputStream.hasNext()) {
+        inputStream.restore(mark);
+        return false;
+      }
+
+      if (inputStream.next() === '\n') {
+        inputStream.restore(mark);
+        return true;
+      }
+    }
+  };
+
+  stream.next = function() {
+    var c = inputStream.next();
+
+    if (c === '/') {
+      var cc = inputStream.peek();
+
+      if (cc !== '/') {
+        return c;
+      }
+
+      // Move over the '/' we just peeked
+      inputStream.next();
+
+      while (true) {
+        var ccc = inputStream.next();
+
+        if (ccc === '\n') {
+          return ccc;
+        }
+      }
+    }
+
+    return c;
+  };
+
+  [
+    'mark',
+    'restore',
+    'describeMark',
+    'describeMarkRange'
+  ].forEach(function(method) {
+    stream[method] = inputStream[method];
+  });
+
+  return stream;
+};
