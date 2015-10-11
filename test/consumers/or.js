49b55f835dbae01ee028d7063f9c5b424640ae46
Andrew Morris <voltrevo@gmail.com>
2015-10-10 19:00:09 -0700

    Add first or test

---
+++ test/consumers/or.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/* global describe it */
+
+// core modules
+var assert = require('assert');
+
+// local modules
+var linestream = require('../../lib/streams/linestream.js');
+var or = require('../../lib/consumers/or.js');
+var single = require('../../lib/consumers/single.js');
+
+describe('or', function() {
+  it('or(a, b) accepts a and b and rejects c in abc', function() {
+    var stream = linestream('test', 'abc');
+    var parser = or(single('a'), single('b'));
+
+    var results = [stream, stream, stream].map(parser);
+
+    assert.deepEqual(
+      results.map(function(result) {
+        return [result.accepted, result.valid];
+      }),
+      [
+        [true, true],  // a accepted and valid
+        [true, true],  // b accepted and valid
+        [false, false] // c not accepted nor valid
+      ]
+    );
+
+    assert.equal(results[0].value.value, 'a');
+    assert.equal(results[1].value.value, 'b');
+  });
+});
