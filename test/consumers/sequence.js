a3126a4bca901818c8bd049ba2778c2e944851ab
Andrew Morris <voltrevo@gmail.com>
2015-10-11 12:12:55 -0700

    Add test for sequence.js

---
+++ test/consumers/sequence.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* global describe it */
+
+// core modules
+var assert = require('assert');
+
+// local modules
+var linestream = require('../../lib/streams/linestream.js');
+var sequence = require('../../lib/consumers/sequence.js');
+var single = require('../../lib/consumers/single.js');
+
+describe('sequence', function() {
+  it('sequence(a, b, c) accepts abc', function() {
+    var stream = linestream('test', 'abc');
+    var parser = sequence(
+      single('a'),
+      single('b'),
+      single('c')
+    );
+
+    var parseResult = parser(stream);
+
+    assert.deepEqual(
+      parseResult.value,
+      ['a', 'b', 'c']
+    );
+  });
+
+  // TODO: Invalidity propagates
+});
