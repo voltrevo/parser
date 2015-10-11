ffd37955167bdf8fbb1bc375c276703ee75022b1
Andrew Morris <voltrevo@gmail.com>
2015-10-10 19:27:39 -0700

    Add test for invalid

---
+++ test/consumers/invalid.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/* global describe it */
+
+// core modules
+var assert = require('assert');
+
+// local modules
+var linestream = require('../../lib/streams/linestream.js');
+var invalid = require('../../lib/consumers/invalid.js');
+var single = require('../../lib/consumers/single.js');
+
+describe('invalid', function() {
+  it('can make the single(a) in abc invalid', function() {
+    var stream = linestream('test', 'abc');
+
+    var parser = invalid(single('a'));
+
+    var parseResult = parser(stream);
+
+    assert.equal(parseResult.accepted, true);
+    assert.equal(parseResult.valid, false);
+    assert.equal(parseResult.value, 'a');
+  });
+});
