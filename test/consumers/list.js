172073d0069fec840e3bc35c04151e3ddb32117c
Andrew Morris <voltrevo@gmail.com>
2015-10-15 12:48:47 -0700

    More list tests

--- test/consumers/list.js
+++ test/consumers/list.js
@@ -6,11 +6,24 @@
 var assert = require('assert');
 
 // local modules
+var invalid = require('../../lib/consumers/invalid.js');
 var list = require('../../lib/consumers/list.js');
 var LineStream = require('../../lib/streams/lineStream.js');
 var single = require('../../lib/consumers/single.js');
 
 describe('list', function() {
+  it('accepts an empty input', function() {
+    var stream = LineStream('test', '');
+
+    var consumer = list(single('z'), single(','));
+
+    var parseResult = consumer(stream);
+
+    assert.equal(parseResult.accepted, true);
+    assert.equal(parseResult.valid, true);
+    assert.deepEqual(parseResult.value, []);
+  });
+
   it('accepts z,z,z', function() {
     var stream = LineStream('test', 'z,z,z');
 
@@ -21,5 +34,46 @@ describe('list', function() {
     assert.equal(parseResult.accepted, true);
     assert.equal(parseResult.valid, true);
     assert.deepEqual(parseResult.value, ['z', 'z', 'z']);
+  });
+
+  it('is invalidated by delimiters', function() {
+    var stream = LineStream('test', 'z,z,z');
+
+    var consumer = list(
+      single('z'),
+      invalid(single(','))
+    );
+
+    var parseResult = consumer(stream);
+
+    assert.equal(parseResult.accepted, true);
+    assert.equal(parseResult.valid, false);
+    assert.deepEqual(parseResult.value, ['z', 'z', 'z']);
+
+    // The tail is invalid.
+    assert.deepEqual(
+      parseResult.invalidations.map(function(invalidation) {
+        return invalidation.reason;
+      }),
+      ['Wrapped consumer invalid.']
+    );
+
+    // Within that tail, there are two delimiters that are invalid.
+    assert.deepEqual(
+      (
+        parseResult.
+        invalidations[0].ref.
+        invalidations[0].ref.
+        invalidations[0].ref.
+        invalidations.
+        map(function(invalidation) {
+          return invalidation.reason;
+        })
+      ),
+      [
+        'many: element 0 is invalid',
+        'many: element 1 is invalid'
+      ]
+    );
   });
 });
