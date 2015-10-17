af73c4ebfba5ad06b79e6c4f547c7506554f11af
Andrew Morris <voltrevo@gmail.com>
2015-10-18 01:07:55 +1100

    Add block

--- test/consumers/block.js
+++ test/consumers/block.js
@@ -6,8 +6,8 @@
 var assert = require('assert');
 
 // local modules
-var andNot = require('../../lib/consumers/andNot.js');
 var block = require('../../lib/consumers/block.js');
+var describeResult = require('../../lib/util/describeResult.js');
 var LineStream = require('../../lib/streams/lineStream.js');
 var single = require('../../lib/consumers/single.js');
 
@@ -22,6 +22,18 @@ describe('block', function() {
     assert.equal(parseResult.accepted, true);
     assert.equal(parseResult.valid, true);
     assert.deepEqual(parseResult.value, []);
+  });
+
+  it('accepts arbitrary internal data', function() {
+    var stream = LineStream('test', '{jkl; we20nv23 hello!}');
+
+    var consumer = block(single('{'), single('}'));
+
+    var parseResult = consumer.consume(stream);
+
+    assert.equal(parseResult.accepted, true);
+    assert.equal(parseResult.valid, true);
+    assert.deepEqual(parseResult.value, 'jkl; we20nv23 hello!'.split(''));
   });
 
   it('accepts nested blocks', function() {
