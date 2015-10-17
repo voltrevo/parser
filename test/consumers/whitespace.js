058780fd8cf47d1a34665acaa6e3a5a1d35d195d
Andrew Morris <voltrevo@gmail.com>
2015-10-17 12:28:35 +1100

    Add whitespace consumer

---
+++ test/consumers/whitespace.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/* global describe it */
+
+// core modules
+var assert = require('assert');
+
+// local modules
+var whitespace = require('../../lib/consumers/whitespace.js');
+var many = require('../../lib/consumers/many.js');
+var Stream = require('../../lib/streams/stream.js');
+
+describe('whitespace', function() {
+  it('accepts whitespace', function() {
+    var stream = Stream(' \n\t  \fabcdef');
+
+    var consumer = many(whitespace);
+
+    var parseResult = consumer(stream);
+
+    assert.equal(parseResult.accepted, true);
+    assert.equal(parseResult.valid, true);
+    assert.deepEqual(parseResult.value, ' \n\t  \f'.split(''));
+  });
+});
