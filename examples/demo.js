4e2031e8d15163f1aad7aa412d181c1212beb1a7
Andrew Morris <voltrevo@gmail.com>
2015-10-17 11:47:53 +1100

    Lint fixes for demo.js

--- examples/demo.js
+++ examples/demo.js
@@ -1,17 +1,17 @@
-"use strict"
+'use strict';
 
-var parser = require("./parser")
-var stream = require("./stream")
-var jsonParser = require("./json/json")
-var tinyCpp = require("./tinyCpp/cppParse")
+var parser = require('../lib/index.js');
+var Stream = require('../lib/streams/stream.js');
+var jsonParser = require('./json/json');
+var tinyCpp = require('./tinyCpp/cppParse');
 
 window.json = function(str) {
-    return jsonParser(new stream(str))
-}
+  return jsonParser(Stream(str));
+};
 
 window.cpp = function(str) {
-    return parser.mustConsumeAll(tinyCpp)(new stream(str))
-}
+  return parser.mustConsumeAll(tinyCpp)(Stream(str));
+};
 
-window.json.impl = jsonParser.impl
-window.stream = stream
+window.json.impl = jsonParser.impl;
+window.stream = Stream;
