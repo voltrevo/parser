aefa43bf37afd69a0603a6f4941ad68ac1c80210
Andrew Morris <voltrevo@gmail.com>
2015-10-17 14:17:42 +1100

    Add wrapOptionalWhitespace

---
+++ test/consumers/wrapOptionalWhitespace.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/* global describe it */
+
+// core modules
+var assert = require('assert');
+
+// local modules
+var digit = require('../../lib/consumers/digit.js');
+var many = require('../../lib/consumers/many.js');
+var Stream = require('../../lib/streams/stream.js');
+var wrapOptionalWhitespace = require('../../lib/consumers/wrapOptionalWhitespace.js');
+
+describe('wrapOptionalWhitespace', function() {
+  it('allows consumers to accept the whitespace surrounding them', function() {
+    var stream = Stream('12 3 45   67 8 9');
+
+    var consumer = many(
+      wrapOptionalWhitespace(digit)
+    );
+
+    var parseResult = consumer(stream);
+
+    assert.equal(parseResult.accepted, true);
+    assert.equal(parseResult.valid, true);
+    assert.deepEqual(parseResult.value, '123456789'.split(''));
+  });
+});
