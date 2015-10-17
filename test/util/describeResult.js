e1c49a58b70e499f4f9403f5c337756c314ebeef
Andrew Morris <voltrevo@gmail.com>
2015-10-17 22:27:38 +1100

    Add describeResult tests for invalidations

--- test/util/describeResult.js
+++ test/util/describeResult.js
@@ -8,22 +8,22 @@ var assert = require('assert');
 // local modules
 var describeResult = require('../../lib/util/describeResult.js');
 
+var mockParseResult = function(accepted, valid) {
+  return {
+    name: 'foo',
+    accepted: accepted,
+    valid: valid,
+    invalidations: [],
+    location: ['a', 'b']
+  };
+};
+
 var mockStream = {
   describeMarkRange: function(markA, markB) { return [markA, markB].join('-'); }
 };
 
 describe('describeResult', function() {
   it('produces appropriate heading when invalidations are empty', function() {
-    var mockParseResult = function(accepted, valid) {
-      return {
-        name: 'foo',
-        accepted: accepted,
-        valid: valid,
-        invalidations: [],
-        location: ['a', 'b']
-      };
-    };
-
     assert.equal(
       describeResult(mockParseResult(true, true), mockStream),
       'foo valid at a-b'
@@ -37,6 +37,45 @@ describe('describeResult', function() {
     assert.equal(
       describeResult(mockParseResult(false, false), mockStream),
       'foo rejected at a-b'
+    );
+  });
+
+  it('includes invalidation reasons', function() {
+    var parseResult = mockParseResult(true, false);
+
+    parseResult.invalidations.push({
+      reason: 'because the test said so'
+    });
+
+    parseResult.invalidations.push({
+      reason: 'because the test said so again'
+    });
+
+    assert.equal(
+      describeResult(parseResult, mockStream),
+      (
+        'foo invalid at a-b:\n' +
+        '  because the test said so\n' +
+        '  because the test said so again'
+      )
+    );
+  });
+
+  it('includes description of reference of invalidation', function() {
+    var parseResult = mockParseResult(false, false);
+
+    parseResult.invalidations.push({
+      reason: 'because the test said so',
+      ref: mockParseResult(false, false)
+    });
+
+    assert.equal(
+      describeResult(parseResult, mockStream),
+      (
+        'foo rejected at a-b:\n' +
+        '  because the test said so:\n' +
+        '    foo rejected at a-b'
+      )
     );
   });
 });
